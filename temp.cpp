a14c1d6055caab8f7600ea2cc29dd472404c9063
ashar-viraj-iitb <25m0756@iitb.ac.in>
2025-10-11 23:54:34 +0530

    Commit check

--- temp.cpp
+++ temp.cpp
@@ -1,25 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define int long long int
-#define rep(i, a, b) for(auto i = a; i < b; i++)
-#define reprev(i, a, b) for (auto i = a; i >= b; i--)
-#define endl '\n'
-#define mp make_pair
-#define um unordered_map
-#define MOD 1000000007
-#define f first
-#define s second
-#define pb push_back
-#define out(valid) cout << (valid ? "YES\n" : "NO\n")
-
-const int x4[4] = {-1, 0, 1, 0}, y4[4] = {0, 1, 0, -1};
-const int x8[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, y8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-
-int32_t main()
+bool is_adjacent(char A, char B)
 {
-    map<int, map<int, map<int, map<int, char>>>> dp;
-    cout << (dp[0][1][2][3] == '\0') << endl;
-    cout << "Hello";
+    return (abs(A - B) == 1); // 'A' 'B' and 'B' 'A' etc
+}
+
+void move_disks(int n, char source, char destination)
+{
+    if (n == 0 || source == destination)
+    {
+        return;
+    }
+    if (is_adjacent(source, destination))
+    {
+        char auxiliary;
+        if ((source == 'A' && destination == 'B') || (source == 'B' && destination == 'A'))
+        {
+            auxiliary = 'C';
+        }
+        else
+        {
+            auxiliary = 'A';
+        }
+        move_disks(n - 1, source, auxiliary);
+        cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+        move_disks(n - 1, auxiliary, destination);
+    }
+    else
+    {
+        move_disks(n - 1, source, destination);
+        cout << "Move disk " << n << " from " << source << " to B" << endl;
+        move_disks(n - 1, destination, source);
+        cout << "Move disk " << n << " from B to " << destination << endl;
+        move_disks(n - 1, source, destination);
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    move_disks(n, 'A', 'B');
     return 0;
 }
+
+/*
+Move disk 1 from A to B
+Move disk 1 from B to C
+Move disk 2 from A to B
+Move disk 1 from C to B
+Move disk 1 from B to A
+Move disk 2 from B to C
+Move disk 1 from A to B
+Move disk 1 from B to C
+Move disk 3 from A to B
+Move disk 1 from C to B
+Move disk 1 from B to A
+Move disk 2 from C to B
+Move disk 1 from A to B
+
+Move disk 1 from B to A
+Move disk 2 from B to C
+Move disk 1 from A to B
+Move disk 1 from B to C
+Move disk 3 from B to A
+Move disk 1 from C to B
+Move disk 1 from B to A
+Move disk 2 from C to B
+Move disk 1 from A to B
+Move disk 1 from B to C
+Move disk 2 from B to A
+Move disk 1 from C to B
+Move disk 1 from B to A
+*/
